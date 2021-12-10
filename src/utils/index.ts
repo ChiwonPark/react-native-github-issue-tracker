b96d814b4295ec6bbd21aa4b35fbc5ec78a3bcd3
ChiwonPark <lordweasel@naver.com>
2021-12-10 18:42:20 +0900

    fix: linkParser 수정 (page 앞에 & 대신 ?가 있는 경우)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,9 +3,9 @@ export const linkParser = (linkHeader: string) => {
   //Link 단위로 split
   const chunk = linkHeader.split(',');
   //마지막 페이지를 가리키는 Link를 찾습니다.
-  const lastLink = chunk.find(e => e.endsWith('"last"'));
+  const lastLink = chunk.find(e => e.endsWith('rel="last"'));
   // example: <https://api.github.com/search/issues?q=is%3Aissue+state%3Aopen+&page=34&sort=created&order=desc>; rel="last"
-  const lastpage = lastLink?.split('&page=')[1].split('>;')[0].split('&')[0];
+  const lastpage = lastLink?.split('page=')[1].split('>;')[0].split('&')[0];
 
   return {
     lastPage: Number(lastpage),
